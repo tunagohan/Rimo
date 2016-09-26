2b1a3aeb2962c2fbb5086c91db6066984c9a809d
tunagohan <kazuxtuna@gmail.com>
2016-09-27 00:31:56 +0900

    server add

---
+++ SERVER/firebase.js
@@ -0,0 +1,21 @@
+var Firebase = require('firebase');
+
+Firebase.initializeApp({
+  databaseURL: "https://rimo-9657d.firebaseio.com"
+});
+
+var db = Firebase.database();
+var ref = db.ref('power');
+
+var exec = require('child_process').exec;
+var command;
+
+ref.on("value", function (snapshot){
+	if(snapshot.val() == "true"){
+	exec("irsend SEND_ONCE LIGHT_IR power");
+	console.log(snapshot.val());
+	} else {
+	exec("irsend SEND_ONCE LIGHT_IR power");
+	console.log(snapshot.val());
+}
+});
