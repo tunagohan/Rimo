92b0e7b91e43f6fb2e7a7aea83a709fdf7ede283
tunagohan <kazuxtuna@gmail.com>
2016-12-21 10:57:10 +0900

    changed
    delete php code.
    added firebase auth

---
+++ HTML/lib/js/Sing.js
@@ -0,0 +1,81 @@
+//Firebase初期設定
+var config = {
+    apiKey: "AIzaSyDwk4syHlJQWrryb-WXU8FW7RB7JXufmtE",
+    authDomain: "rimo-8666d.firebaseapp.com",
+    databaseURL: "https://rimo-8666d.firebaseio.com",
+    storageBucket: "rimo-8666d.appspot.com",
+    messagingSenderId: "273572213277"
+};
+firebase.initializeApp(config);
+
+
+
+//DOM取得
+var inputarea = document.getElementById('input-area');
+var newuser = document.getElementById('newuser');
+var login = document.getElementById('login');
+var logout = document.getElementById('logout');
+var info = document.getElementById('info');
+
+
+
+//新規登録処理
+newuser.addEventListener('click', function(e) {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    alert('登録できません（' + error.message + '）');
+  });
+});
+
+
+
+//ログイン処理
+login.addEventListener('click', function(e) {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    alert('ログインできません（' + error.message + '）');
+  });
+});
+
+
+
+//ログアウト処理
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+});
+
+
+
+//認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    loginDisplay();
+  }
+  else {
+    logoutDisplay();
+  }
+});
+
+
+
+function loginDisplay() {
+  logout.classList.remove('hide');
+  inputarea.classList.add('hide');
+
+  info.textContent = "ログイン中です";
+	document.location = "../Remote/index.html";
+}
+
+
+function logoutDisplay() {
+  logout.classList.add('hide');
+  inputarea.classList.remove('hide');
+
+  info.textContent = "";
+}
---
+++ HTML/lib/js/Singkeep.js
@@ -0,0 +1,33 @@
+
+var config = {
+    apiKey: "AIzaSyDwk4syHlJQWrryb-WXU8FW7RB7JXufmtE",
+    authDomain: "rimo-8666d.firebaseapp.com",
+    databaseURL: "https://rimo-8666d.firebaseio.com",
+    storageBucket: "rimo-8666d.appspot.com",
+    messagingSenderId: "273572213277"
+};
+firebase.initializeApp(config);
+
+var inputarea = document.getElementById('input-area');
+var newuser = document.getElementById('newuser');
+var login = document.getElementById('login');
+var logout = document.getElementById('logout');
+var info = document.getElementById('info');
+
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    
+  }
+  else {
+    logoutDisplay();
+  }
+});
+
+function logoutDisplay() {
+document.location = "../index.html";
+
+}
